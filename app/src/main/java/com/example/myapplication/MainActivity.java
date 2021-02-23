a92ff9b7800adccbe8e82f0e2aa16bafd419766e
ADDOUCHE Walid <walid.addouche@etu.univ-amu.fr>
2021-02-23 01:11:33 +0100

    qlq trucs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonmultiply;
+    Button buttondivide;
+    Button buttonsub;
+    Button buttonadd;
+    EditText result;
+    int compteur = 0 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        this.buttonadd = findViewById(R.id.buttonadd);
+        this.result = findViewById(R.id.result);
+        buttonadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compteur ++;
+                System.out.println(compteur);
+                result.setText(compteur);
+            }
+        });
+
+            }
+
+
 }
